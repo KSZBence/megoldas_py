efc38d3584495d9d835947b3d07a837d817996f6
KSZBence <kunszekelybence@gmail.com>
2023-02-01 12:26:31 +0100

    összpont függvény

--- eredmeny.py
+++ eredmeny.py
@@ -1,7 +1,16 @@
 #megoldás
 def eredmeny(jatekospont, geppont):
+    jatekospont = osszpont(jatekospont)
+    geppont = osszpont(geppont)
+
     if jatekospont > 21:
         print("vesztettél")
     elif geppont > 21:
         print("nyertél")
 #teszteset
+
+def osszpont(lista):
+    osszeg = 0
+    for i in lista:
+        osszeg += i
+    return osszeg
