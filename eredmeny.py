df64f4576e69fcc3dbab5078a2d973710926299b
KSZBence <kunszekelybence@gmail.com>
2023-02-01 12:35:21 +0100

    elnevezes javitasok

--- eredmeny.py
+++ eredmeny.py
@@ -1,16 +1,18 @@
 #megoldás
-def eredmeny(jatekospont, geppont):
-    jatekospont = osszpont(jatekospont)
-    geppont = osszpont(geppont)
+def eredmeny(jatekoslapok, geplapok):
+    jatekospont = osszpont(jatekoslapok)
+    geppont = osszpont(geplapok)
 
     if jatekospont > 21:
         print("vesztettél")
     elif geppont > 21:
         print("nyertél")
-#teszteset
+
 
 def osszpont(lista):
     osszeg = 0
     for i in lista:
         osszeg += i
     return osszeg
+
+#teszteset
