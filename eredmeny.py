0689d14a0e8cea08106c6aa2bbccdb626f18c00d
KSZBence <kunszekelybence@gmail.com>
2023-02-02 13:17:17 +0100

    mindenki vesztett

--- eredmeny.py
+++ eredmeny.py
@@ -4,7 +4,7 @@ def eredmeny(jatekoslapok:list, geplapok:list):
     geppont = osszpont(geplapok)
 
     if jatekospont > 21 and geppont > 21:
-        return "mindenki vesztett"
+        return "dontetlen"
     elif jatekospont > 21:
         return "jatekos vesztett"
     elif geppont > 21:
@@ -32,6 +32,16 @@ def gep_vesztett_teszt():
     jatekoslapok = [10,5,8]
     geplapok = [10,5, 10]
     print(eredmeny(jatekoslapok, geplapok))
+    
+def mindenki_vesztett_dontetlen_teszt():
+    jatekoslapok = [10,9,5]
+    geplapok = [10, 8, 5]
+    vart = "dontetlen"
+    kapott = eredmeny(jatekoslapok, geplapok)
+    if vart == kapott:
+        print("Teszteset sikeres.")
+    else:
+        print("Teszteset sikertelen.")
     
 def jatekos_nyert_teszt():
     jatekoslapok = [10,9]
@@ -67,13 +77,14 @@ def jatekos_nyert_lapszammal_teszt():
 
 def gep_nyert_lapszammal_teszt():
     jatekoslapok = [10, 4, 5]
-    geplapok = [10, 10]
+    geplapok = [10, 9]
     vart = "gep nyert"
     kapott = eredmeny(jatekoslapok, geplapok)
     if vart == kapott:
         print("Teszteset sikeres.")
     else:
         print("Teszteset sikertelen.")
+
 
 def dontetlen_21_alatt_teszt():
     jatekoslapok = [10, 3, 5]
@@ -85,9 +96,18 @@ def dontetlen_21_alatt_teszt():
     else:
         print("Teszteset sikertelen.")
 
+
 def tesztek():
     jatekos_vesztett_teszt()
     gep_vesztett_teszt()
+    mindenki_vesztett_dontetlen_teszt()
+    jatekos_nyert_teszt()
+    gep_nyert_teszt()
+    jatekos_nyert_lapszammal_teszt()
+    gep_nyert_lapszammal_teszt()
+    dontetlen_21_alatt_teszt()    
+    
+    
 
 
 tesztek()
