3e49e5a6c3a36bfe2f36a052dff45617c70d50a9
KSZBence <kunszekelybence@gmail.com>
2023-02-02 12:54:15 +0100

    nyert tesztesetek

--- eredmeny.py
+++ eredmeny.py
@@ -32,6 +32,58 @@ def gep_vesztett_teszt():
     jatekoslapok = [10,5,8]
     geplapok = [10,5, 10]
     print(eredmeny(jatekoslapok, geplapok))
+    
+def jatekos_nyert_teszt():
+    jatekoslapok = [10,9]
+    geplapok = [10, 7]
+    vart = "jatekos nyert"
+    kapott = eredmeny(jatekoslapok, geplapok)
+    if vart == kapott:
+        print("Teszteset sikeres.")
+    else:
+        print("Teszteset sikertelen.")
+
+
+def gep_nyert_teszt():
+    jatekoslapok = [10,7]
+    geplapok = [10, 10]
+    vart = "gep nyert"
+    kapott = eredmeny(jatekoslapok, geplapok)
+    if vart == kapott:
+        print("Teszteset sikeres.")
+    else:
+        print("Teszteset sikertelen.")
+
+def jatekos_nyert_lapszammal_teszt():
+    jatekoslapok = [10, 9]
+    geplapok = [10, 4, 5]
+    vart = "jatekos nyert"
+    kapott = eredmeny(jatekoslapok, geplapok)
+    if vart == kapott:
+        print("Teszteset sikeres.")
+    else:
+        print("Teszteset sikertelen.")
+
+
+def gep_nyert_lapszammal_teszt():
+    jatekoslapok = [10, 4, 5]
+    geplapok = [10, 10]
+    vart = "gep nyert"
+    kapott = eredmeny(jatekoslapok, geplapok)
+    if vart == kapott:
+        print("Teszteset sikeres.")
+    else:
+        print("Teszteset sikertelen.")
+
+def dontetlen_21_alatt_teszt():
+    jatekoslapok = [10, 3, 5]
+    geplapok = [10, 4, 4]
+    vart = "dontetlen"
+    kapott = eredmeny(jatekoslapok, geplapok)
+    if vart == kapott:
+        print("Teszteset sikeres.")
+    else:
+        print("Teszteset sikertelen.")
 
 def tesztek():
     jatekos_vesztett_teszt()
@@ -39,3 +91,4 @@ def tesztek():
 
 
 tesztek()
+
